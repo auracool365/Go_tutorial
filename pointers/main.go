1c98ec056278a5763c21c643e8b04419b11c6afb
Cornelius <corneliusisaac62@gmail.com>
2025-11-17 02:55:19 +0100

    modified pointers

--- pointers/main.go
+++ pointers/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 // Pass by value
-func increment_1(arr [5]uint8) [5]uint8 {
+func increment1(arr [5]uint8) [5]uint8 {
 	for i := range len(arr) {
 		arr[i]++
 	}
@@ -12,7 +12,7 @@ func increment_1(arr [5]uint8) [5]uint8 {
 }
 
 // Pass by reference
-func increment_2(arr *[5]uint8) [5]uint8 {
+func increment2(arr *[5]uint8) [5]uint8 {
 	for i := range len(arr) {
 		arr[i]++
 	}
@@ -20,73 +20,73 @@ func increment_2(arr *[5]uint8) [5]uint8 {
 	return *arr
 }
 
-func change_name(user_name *string) *string {
-	*user_name = "Johnny"
+func changeName(userName *string) *string {
+	*userName = "Johnny"
 
-	return user_name
+	return userName
 }
 
 func main() {
-	var my_ptr *int32   // Declared but not initialized, pointer points to nil
-	fmt.Println(my_ptr) // nil
+	var myPtr *int32   // Declared but not initialized, pointer points to nil
+	fmt.Println(myPtr) // nil
 	// fmt.Println(my_ptr) // Error! trying to read value address that doesn't exist, since my_ptr has not point to any address
 
-	var my_var int32 = 10
-	my_ptr = &my_var
-	fmt.Println("The value is", my_var, "at address", my_ptr)
+	var myVar int32 = 10
+	myPtr = &myVar
+	fmt.Println("The value is", myVar, "at address", myPtr)
 
 	// Dereferencing
-	*my_ptr = 20
-	fmt.Println("The value is", *my_ptr, "at address", my_ptr)
+	*myPtr = 20
+	fmt.Println("The value is", *myPtr, "at address", myPtr)
 
 	// Initialize a pointer with new() and take memory space to hold value
-	var user_name *string = new(string)
-	fmt.Println("The value is", *user_name, "at address", user_name) // No value at the moment, but the pointer points to a legit address
+	var userName *string = new(string)
+	fmt.Println("The value is", *userName, "at address", userName) // No value at the moment, but the pointer points to a legit address
 
-	*user_name = "Cornelius" // writes a value to that address
-	fmt.Println("The value is", *user_name, "at address", user_name)
+	*userName = "Cornelius" // writes a value to that address
+	fmt.Println("The value is", *userName, "at address", userName)
 
 	// Initialize with a variable address
-	is_go_fun := true
-	var is_go_fun_ptr *bool = &is_go_fun
-	fmt.Println("The value is", is_go_fun, "at address", is_go_fun_ptr)
+	isGoFun := true
+	var isGoFunPtr *bool = &isGoFun
+	fmt.Println("The value is", isGoFun, "at address", isGoFunPtr)
 
-	// If a short hand syntax variable is used, then it is better to also use short hand for a pointer, to avoid type error
+	// If a shorthand syntax variable is used, then it is better to also use short hand for a pointer, to avoid type error
 	a := 3.142
-	a_ptr := &a
-	fmt.Println("The value is", a, "at address", a_ptr)
+	aPtr := &a
+	fmt.Println("The value is", a, "at address", aPtr)
 
 	// Reassigning pointers
 	var x float32 = 20.001
-	var x_ptr *float32 = &x
-	fmt.Println("The value is", x, "at address", x_ptr)
+	var xPtr *float32 = &x
+	fmt.Println("The value is", x, "at address", xPtr)
 
 	var y float32 = 30.002
-	x_ptr = &y
-	fmt.Println("The value is", y, "at address", x_ptr)
+	xPtr = &y
+	fmt.Println("The value is", y, "at address", xPtr)
 	fmt.Println()
 
 	// Function with pointers
 	// Copying (pass by value) leading to different address
 	nums := [5]uint8{1, 2, 3, 4, 5}
 	fmt.Printf("%d Array address in main() %p\n", nums, &nums)
-	fmt.Println(increment_1(nums)) // Incremented copies inside increment_1()
-	fmt.Println(nums)              // The original elements remained unchanged
+	fmt.Println(increment1(nums)) // Incremented copies inside increment_1()
+	fmt.Println(nums)             // The original elements remained unchanged
 	fmt.Println()
 
 	// pass by reference (no copies), same address
 	fmt.Printf("%d Array address in main() %p\n", nums, &nums)
-	fmt.Println(increment_2(&nums)) // Accepts the memory address of the array and modifies the original elements of the array
-	fmt.Println(nums)               // The original elements show the change
+	fmt.Println(increment2(&nums)) // Accepts the memory address of the array and modifies the original elements of the array
+	fmt.Println(nums)              // The original elements show the change
 
 	name := "Tom"
 	fmt.Println(name)
-	change_name(&name)
+	changeName(&name)
 	fmt.Println(name)
 
-	name_1 := "Ade"
-	fmt.Println(name_1)
-	change_name(&name_1)
-	fmt.Println(name_1)
+	name1 := "Ade"
+	fmt.Println(name1)
+	changeName(&name1)
+	fmt.Println(name1)
 
 }
